e38c9286a39b5f72e86144ec6977f32d086b410d
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-06 12:24:15 -0400

    blank line added

--- code/src/parallel_processing.py
+++ code/src/parallel_processing.py
@@ -20,11 +20,13 @@ if __name__ == "__main__":
        console.print(p.map(pool_second_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
        console.print(p.map(pool_third_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
     end_time = timeit.default_timer()
-    console.print(f"Execution time: {end_time - start_time} seconds.")
+    console.print(f"Execution time with 5 cores: {end_time - start_time} seconds.")
+
+    console.print("")
 
     second_start_time = timeit.default_timer()
     with Pool(1) as p:
        console.print(p.map(pool_second_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
        console.print(p.map(pool_third_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
     second_end_time = timeit.default_timer()
-    console.print(f"Execution time: {second_end_time - second_start_time}")
+    console.print(f"Execution time with 1 core: {second_end_time - second_start_time}")

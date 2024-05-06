e1fa655a1d888db8b5c370c2ad630b9183906344
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-06 11:39:52 -0400

    more experiment added for better understanding

--- code/src/parallel_processing.py
+++ code/src/parallel_processing.py
@@ -5,9 +5,14 @@ from rich.console import Console
 
 console = Console()
 
-def pool(x: int) -> int:
+def pool_second_power(x: int) -> int:
     return x*x
+
+def pool_third_power(x: int) -> int:
+    return x*x*x
+
 
 if __name__ == "__main__":
     with Pool(5) as p:
-       console.print(p.map(pool, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+       console.print(p.map(pool_second_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+       console.print(p.map(pool_third_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))

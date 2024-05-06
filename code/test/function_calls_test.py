bdd56efce745dad3b5a1ade2dc5c9df72a84ee3c
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-06 19:12:26 -0400

    changes

--- code/test/function_calls_test.py
+++ code/test/function_calls_test.py
@@ -3,3 +3,18 @@
 import unittest
 from src.function_calls import OPTFunction
 
+class TestOPTFunction(unittest.TestCase):
+    def setUp(self):
+        self.opt_function = OPTFunction()
+        self.oddList = []
+        self.evenList = []
+
+    def test_optimized_function(self):
+        self.opt_function.optimized_function(self.oddList, self.evenList)
+        self.assertEqual(len(self.oddList), 100000)  # 10 odd numbers * 10000
+        self.assertEqual(len(self.evenList), 100000)  # 10 even numbers * 10000
+        self.assertTrue(all(i % 2 != 0 for i in self.oddList))
+        self.assertTrue(all(i % 2 == 0 for i in self.evenList))
+
+if __name__ == "__main__":
+    unittest.main()

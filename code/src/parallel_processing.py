eaa4ee14cce1a7e7e698b4a6bc69231ecfda83d0
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-06 11:15:42 -0400

    parallel processing example added

--- code/src/parallel_processing.py
+++ code/src/parallel_processing.py
@@ -1,1 +1,13 @@
 """Leveraging parallel processing with python libraries."""
+
+from multiprocessing import Pool
+from rich.console import Console
+
+console = Console()
+
+def pool(x: int) -> int:
+    return x*x
+
+if __name__ == "__main__":
+    with Pool(5) as p:
+       console.print(p.map(pool, [1, 2, 3, 4, 5, 6, 7, 8, 9]))

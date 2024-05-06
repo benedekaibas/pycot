642f4a4b68b30faeb3a198d0320317f568572828
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-05 20:14:32 -0400

    code added

--- code/src/function_calls.py
+++ code/src/function_calls.py
@@ -1,5 +1,17 @@
 """Optimizing function calls."""
 
+
+myList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] * 1000
+oddList = []
+evenList = []
+
+for i in myList:
+    if i % 2 != 0:
+        oddList.append(i)
+    else:
+        evenList.append(i)
+
+
 class OPTFunction:
     """Class for doing research in optimizing function calls."""
 

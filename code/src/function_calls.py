d80e730845c07bfd321132c951f78055be06a36d
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-05 20:56:35 -0400

    done

--- code/src/function_calls.py
+++ code/src/function_calls.py
@@ -22,11 +22,7 @@ print(f"Execution time: {end_time - start_time} seconds")
 class OPTFunction:
     """Class for doing research in optimizing function calls."""
 
-    def __init__(self) -> None:
-        """Init function for the class."""
-        pass
-
-    def optimized_function(oddList: List, evenList: List, start_time: float, end_time: float) -> List:
+    def optimized_function(self, oddList: List, evenList: List) -> str:
         myList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] * 1000
 
         start_time = timeit.default_timer()
@@ -38,3 +34,11 @@ class OPTFunction:
                 evenList.append(i)
 
         end_time = timeit.default_timer()
+
+        return f"Execution time: {end_time - start_time} seconds"
+
+if __name__ == "__main__":
+    opt_function = OPTFunction()
+    oddList = []
+    evenList = []
+    print(opt_function.optimized_function(oddList, evenList))

f9eff942ea59d8c8e526652e8a30303aabc7efb9
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-06 12:22:31 -0400

    using multiple multiprocessor methods

--- code/src/parallel_processing.py
+++ code/src/parallel_processing.py
@@ -21,3 +21,10 @@ if __name__ == "__main__":
        console.print(p.map(pool_third_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
     end_time = timeit.default_timer()
     console.print(f"Execution time: {end_time - start_time} seconds.")
+
+    second_start_time = timeit.default_timer()
+    with Pool(1) as p:
+       console.print(p.map(pool_second_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+       console.print(p.map(pool_third_power, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    second_end_time = timeit.default_timer()
+    console.print(f"Execution time: {second_end_time - second_start_time}")

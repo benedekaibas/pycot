d25b495de2c687e3a74b38acf754d24df1edcec0
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-11 21:46:50 -0400

    changes

--- code/pycot_src/parallel_processing_complex.py
+++ code/pycot_src/parallel_processing_complex.py
@@ -14,12 +14,15 @@ class LPPComplex:
 
     def pool_size(self) -> List[int]:
         """Iterating through the pool sizes."""
-        #second_power = pool_second_power() # we might use it in a different way
+        #range of possible pool sizes
         pool = [1,2,3,4,5]
         for i in pool:
             return Pool(i)
 
-
+    def pool_second_power(self, number: int) -> int:
+        """Raising the parameter to its second power."""
+        return number ** number
+    
 if __name__ == "__main__":
     lpp_complex = LPPComplex()
     console.print(lpp_complex.pool_size())

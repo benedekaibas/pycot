ed4fe0a9040aacc36323c3db46b2d10d43046fa8
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-13 13:27:40 -0400

    TODOs added

--- code/pycot_src/parallel_processing_complex.py
+++ code/pycot_src/parallel_processing_complex.py
@@ -5,6 +5,10 @@ from multiprocessing import Pool
 from rich.console import Console
 from typing import List
 
+# TODO: iterate through the possible pool sizes
+# TODO: create a range that we can perform a doubling experiment to its every values
+# TODO: for each pool size return the result of the iteration with the doubling function
+
 
 console = Console()
 
@@ -12,18 +16,4 @@ class LPPComplex:
     def __init__(self) -> None:
         pass
 
-    def pool_size(self) -> List[int]:
-        """Iterating through the pool sizes."""
-        #range of possible pool sizes
-        pool_range = range(0,1000)
-        for i in pool_range:
-            return Pool(i)
 
-    def pool_second_power(self, number: int) -> int:
-        """Raising the parameter to its second power."""
-        return number ** number
-   
-
-if __name__ == "__main__":
-    lpp_complex = LPPComplex()
-    console.print(lpp_complex.pool_size())

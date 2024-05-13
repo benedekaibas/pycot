bef80b62531e95e18a6a358497bd98912bd48f82
benedekaibas <kaibas.benedek02@gmail.com>
2024-05-13 15:21:45 -0400

    got result for the experiment

--- code/pycot_src/parallel_processing_complex.py
+++ code/pycot_src/parallel_processing_complex.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 from rich.console import Console
 from typing import List
 
-# TODO: iterate through the possible pool sizes
+# TODO: iterate through the possible pool sizes - DONE
 # TODO: create a range that we can perform a doubling experiment to its every values
 # TODO: for each pool size return the result of the iteration with the doubling function
 
@@ -19,7 +19,18 @@ class LPPComplex:
     def cpu_cores(self):
         """Returning all the cores that laptops could have."""
         return [2,3,4,5,6,7,8]
+    
+    def experiment(self):
+        """Do experiment for every single core and return the experiment"""
+        cores = self.cpu_cores()
+
+        for item in cores:
+            console.print(f"{item} in for {cores} core: {item ** item}")
+
+
+
+
 
 if __name__ == "__main__":
     lpp_complex =  LPPComplex()
-    console.print(lpp_complex.pool_size())
+    console.print(lpp_complex.experiment())
